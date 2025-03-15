ac7a8ec3fbf1526b26498109739ab3928564edef
vTurmina <vitor.turmina@catolicasc.edu.br>
2025-03-15 14:38:59 -0300

    Add files via upload

---
+++ Atividade_03_fundamentos_14-03/Vitor_Turmina/2_vitor_turmina.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main(void) {
+
+  int idade;
+
+  printf("Informe a sua idade\n");
+  scanf("%d", &idade);
+
+  printf("Voce tem %d anos\n", idade);
+
+  return 0;
+}
---
+++ Atividade_03_fundamentos_14-03/Vitor_Turmina/3_vitor_turmina.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main(void) {
+
+  int ano;
+
+  printf("Informe o ano atual\n");
+  scanf("%d", &ano);
+
+  printf("Otimo %d!\n", ano);
+
+  return 0;
+}
