bffb38737aa4bd2f51cecd4563396c56dbdedb7f
FernandoHoffmann0201 <fernando07.silva@catolicasc.edu.br>
2025-03-28 21:03:16 -0300

    Add files via upload

---
+++ Atividade_04_estrutura_selecao/Fernando_Hoffmann/Fernando_Hoffmann_4.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+int main(void) {
+  int num1, num2;
+  printf("Digite o primeiro número:\n");
+  scanf("%d", &num1);
+  printf("Digite o segundo número:\n");
+  scanf("%d", &num2);
+  if(num1 > num2)
+    printf (">> %d é maior que %d\n", num1, num2);
+
+  else if (num1 == num2)
+    printf (">> %d é igual a %d\n", num1, num2);
+     
+  else
+    printf (">> %d é maior que %d\n", num2, num1);
+  
+  
+    
+  return 0;
+}
