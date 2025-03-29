66fbf71c4fb9275cb9a396575dadf510ad52cba6
ThauaBrandaoSC <thaua.silva@catolicasc.edu.br>
2025-03-28 21:50:28 -0300

    Add files via upload

---
+++ Atividade_04_estrutura_selecao/Thaua_Brandao/Thaua_Brandao_6.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+int main(void) {
+  int num1, num2, num3;
+  printf("Digite o primeiro número:\n");
+  scanf("%d", &num1);
+  printf("Digite o segundo número:\n");
+  scanf("%d", &num2);
+  printf("Digite o terceiro número:\n");
+  scanf("%d", &num3);
+  if(num1 > num2 && num1 > num3)
+    printf (">> %d é o maior número\n", num1);
+  else if (num2 > num1 && num2 > num3)
+    printf (">> %d é o maior número\n", num2);
+  else 
+    printf (">> %d é o maior número\n", num3);
+  
+  
+  
+  
+  
+    
+  return 0;
+}
