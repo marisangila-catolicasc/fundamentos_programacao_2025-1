dd38f2357f521e60c0acb6bc94e425040eaec63a
ispanholito-catolica <ruan.amaral@catolicasc.edu.br>
2025-04-04 18:45:42 -0300

    Update 6_Ruanp.c

--- Atividade_04_estrutura_selecao/Ruan p/6_Ruanp.c
+++ Atividade_04_estrutura_selecao/Ruan p/6_Ruanp.c
@@ -1,23 +1,23 @@
 #include <stdio.h>
 
 int main() {
-    int x, y, z;
+    int N1, N2, N3;
 
-    printf("Informe o valor de um lado: ");
-    scanf("%d", &x);
+    printf("Informe o primeiro número: ");
+    scanf("%d", &N1);
 
-    printf("Informe o valor de um lado: ");
-    scanf("%d", &y);
+    printf("Informe o segundo número: ");
+    scanf("%d", &N2);
 
-    printf("Informe o valor de um lado: ");
-    scanf("%d", &z);
+    printf("Informe o terceiro número: ");
+    scanf("%d", &N3);
 
-    if (x == y && y == z) {
-        printf("O triângulo é equilátero!\n");
-    } else if (x == y || y == z || z == x) {
-        printf("O triângulo é isósceles!\n");
-    } else {
-        printf("O triângulo é escaleno!\n");
+    if (N1 > N2 && N1 > N3) {
+        printf("O maior número é: %d\n", N1);
+    } else if (N2 > N1 && N2 > N3) {
+        printf("O maior número é: %d\n", N2);
+    } else if (N3 > N1 && N3 > N2) {
+        printf("O maior número é: %d\n", N3);
     }
 
     return 0;
