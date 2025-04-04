a799583d3aa2c0fbcd6f28f8e1928f165d22e25c
ENZO CAMELO VIEIRA <enzo.vieira@catolicasc.edu.br>
2025-04-04 20:14:48 -0300

    Add files via upload

---
+++ Atividade_05_estrutura_selecao_multipla/Enzo_Camelo/3_Enzo_Camelo.c
@@ -0,0 +1,54 @@
+#include <stdio.h>
+
+int main() {
+  float n1, n2, resultado;
+  char operacao;
+  
+  printf("Digite o primeiro número: ");
+  scanf("%f", &n1);
+  
+  printf("Digite o segundo número: ");
+  scanf("%f", &n2);
+
+  printf("Digite a operação: \n");
+  printf("[+] - Soma\n");
+  printf("[-] - Subtração\n");
+  printf("[/] - Divisão\n");
+  printf("[*] - Multiplicação\n");
+  scanf(" %c", &operacao);
+  
+  switch (operacao)
+  {
+    case '+':
+      resultado = n1 + n2;
+      printf("O resultado é: %.2f\n", resultado);
+      break;
+
+    case '-':
+      resultado = n1 - n2;
+      printf("O resultado é: %.2f\n", resultado);
+      break;
+
+    case '/':
+      if (n2 == 0) {
+        printf("O divisor não pode ser 0\n");
+      }
+        
+      else {
+        resultado = n1 / n2;
+        printf("O resultado é: %.2f\n", resultado);
+      }
+      break;
+
+    case '*':
+      resultado = n1 * n2;
+      printf("O resultado é: %.2f\n", resultado);
+      break;
+
+    default:
+      printf("Operação inválida\n");
+  }
+
+
+  return 0;
+}
