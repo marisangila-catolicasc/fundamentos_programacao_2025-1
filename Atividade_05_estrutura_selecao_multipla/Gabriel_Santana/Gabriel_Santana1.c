0ab6e37cedce8aac6f66b2255767421e7f3cd196
ThauaBrandaoSC <thaua.silva@catolicasc.edu.br>
2025-04-04 20:22:43 -0300

    Add files via upload

---
+++ Atividade_05_estrutura_selecao_multipla/Gabriel_Santana/Gabriel_Santana1.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+int main(void) {
+  int codigo;
+  printf("Digite o codigo do produto: ");
+  scanf("%d", &codigo);
+  switch (codigo)
+    {
+  case 1254:
+    printf("Sanduiche de Presunto - R$ 8.00\n");
+    break;
+  case 5698:
+    printf("Churros - R$ 3.50\n");
+    break;
+  case 7114:
+    printf("Suco de limão que parece de limão mas tem gosto de tamarindo - R$ 5.00\n");
+    break;
+    }
+  
+  
+  
+}
