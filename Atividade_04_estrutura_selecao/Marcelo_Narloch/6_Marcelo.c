2dfe9a0901721b5569096e35dd52cdd06aebdf0d
Marcelonarloch <marcelo.pereira@catolicasc.edu.br>
2025-04-04 11:39:41 -0300

    Update 6_Marcelo.c

--- Atividade_04_estrutura_selecao/Marcelo_Narloch/6_Marcelo.c
+++ Atividade_04_estrutura_selecao/Marcelo_Narloch/6_Marcelo.c
@@ -12,12 +12,11 @@ printf("Informe o terceiro numero:");
 scanf("%d", &N3);
   
 if(N1 > N2 && N1 > N3){
-printf("O maior numero é: %d", N1);
+printf("O maior numero eh: %d", N1);
 }else if(N2 > N1 && N2 > N3){
-printf("O maior numero é: %d", N2);
+printf("O maior numero eh: %d", N2);
 }else if(N3 > N1 && N3 > N2){
-printf("O maior numero é: %d", N3);
+printf("O maior numero eh: %d", N3);
 }
-
 return 0;
 }
