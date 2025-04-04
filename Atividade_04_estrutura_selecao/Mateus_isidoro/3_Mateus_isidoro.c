5000b1a1305927af2a16a997c74702ed05de9688
MateusIsidoroo <mateus.isidoro@catolicasc.edu.br>
2025-04-03 21:56:22 -0300

    Update 3_Mateus_isidoro.c

--- Atividade_04_estrutura_selecao/Mateus_isidoro/3_Mateus_isidoro.c
+++ Atividade_04_estrutura_selecao/Mateus_isidoro/3_Mateus_isidoro.c
@@ -5,16 +5,16 @@ int main() {
     float valor_final;
     float valor1 = 2.30;
     float valor2 = 1.95;
-    printf("Digite a quanidade de maçãs\n");
+    printf("Digite a quanidade de maças\n");
     scanf("%d", &quanidade_maca);
     if(quanidade_maca >= 12){
-      printf("O valor da venda é de %.2f\n", valor2);
+      printf("O valor da venda e de %.2f\n", valor2);
       valor_final = quanidade_maca * valor2;
-      printf("O valor total da compra é de %.2f", valor_final);
+      printf("O valor total da compra e de %.2f", valor_final);
     }else{
-      printf("O valor da venda é de %.2f\n", valor1);
+      printf("O valor da venda e de %.2f\n", valor1);
       valor_final = quanidade_maca * valor1;
-      printf("O valor total da compra é de %.2f", valor_final);
+      printf("O valor total da compra e de %.2f", valor_final);
     }
     return 0;
 }
