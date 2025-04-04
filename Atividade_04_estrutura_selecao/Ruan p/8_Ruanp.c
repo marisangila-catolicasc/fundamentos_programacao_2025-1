4b4f272777423cbced7a01a302816d86d418af6a
ispanholito-catolica <ruan.amaral@catolicasc.edu.br>
2025-04-04 18:46:08 -0300

    Create 8_Ruanp.c

---
+++ Atividade_04_estrutura_selecao/Ruan p/8_Ruanp.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main() {
+    int ano;
+
+    printf("Digite o ano: ");
+    scanf("%d", &ano);
+
+    if ((ano % 4 == 0 && ano % 100 != 0) || ano % 400 == 0) {
+        printf("O ano é bissexto!\n");
+    }
+
+    return 0;
+}
