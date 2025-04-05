51ff6ffc898ffd70025a31ecdb8a95b3d031b213
vitor-y83 <jose03.machado@catolicasc.edu.br>
2025-04-04 22:08:05 -0300

    Add files via upload

---
+++ Atividade_05_estrutura_selecao_multipla/Jose_Vitor/2_jose_vitor.c
@@ -0,0 +1,54 @@
+#include <stdio.h>
+
+int main(){
+    
+    int estado;
+    
+    printf("Digite um estado: \n");
+    printf(" [1]Acre AC\n [2]Alagoas AL\n [3]Amapá AP\n [4]Amazonas AM\n [5]Bahia BA\n [6]Ceará CE\n [7]Espírito Santo ES\n [8]Goiás GO\n [9]Maranhão MA\n [10]Mato Grosso MT\n [11]Mato Grosso do Sul MS\n [12]Minas Gerais MG\n [13]Pará PA\n [14]Paraíba PB\n [15]Paraná PR\n [16]Pernambuco PE\n [17]Piauí PI\n [18]Rio de Janeiro RJ\n [19]Rio Grande do Norte RN\n [20]Rio Grande do Sul RS\n [21]Rondônia RO\n [22]Roraima RR\n [23]Santa Catarina SC\n [24]São Paulo SP\n [25]Sergipe SE\n [26]Tocantins TO\n");
+    scanf("%d", &estado);
+    
+    switch(estado){
+        case 23:
+        case 15:
+        case 20:
+            printf("\ne um estado da regiao sul");
+            break;
+        case 24:
+        case 18:
+        case 7:
+        case 12:
+            printf("\ne um estado da regiao sudeste");
+            break;
+        case 8:
+        case 10:
+        case 11:
+            printf("\ne da regiao centro oeste");
+            break;
+        case 9:
+        case 17:
+        case 5:
+        case 6:
+        case 19:
+        case 14:
+        case 16:
+        case 2:
+        case 25:
+            printf("\ne um estado da regiao nordeste");
+            break;
+        case 26:
+        case 13:
+        case 3:
+        case 22:
+        case 4:
+        case 21:
+        case 1:
+            printf("\ne um estado da regiao norte");
+            break;
+        
+        default:
+            printf("Codigo invalido, tente novamente");
+            break;
+    }
+    return 0;
+}
