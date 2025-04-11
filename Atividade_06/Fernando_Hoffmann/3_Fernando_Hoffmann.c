e0272c5c5327e7717c76414e0d5f8264fd8edca8
FernandoHoffmann0201 <fernando07.silva@catolicasc.edu.br>
2025-04-11 20:46:55 -0300

    Add files via upload

---
+++ Atividade_06/Fernando_Hoffmann/3_Fernando_Hoffmann.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+int main(void) {
+  int i;
+  for(i = 0 ; i <= 100 ; i+=5)
+    printf("%d\n", i);
+  
+  return 0;
+}
---
+++ Atividade_06/Fernando_Hoffmann/8_Fernando_Hoffmann.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main() {
+  int numero, a = 0;
+    for (int i = 0; i <= 10; i++) {
+        printf("%d\n- Digite um número: ", i);
+        scanf("%d", &numero);
+        if (numero > a) {
+            a = numero;
+        }
+    }
+  printf(" \nMaior número: %d", a);
+    return 0;
+}
