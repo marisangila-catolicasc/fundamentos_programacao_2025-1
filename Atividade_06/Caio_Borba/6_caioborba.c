58bd86bccc369bffed62354a1540bdd31f7b6410
CaioBorbaa <caio.borba@catolicasc.edu.br>
2025-04-25 16:08:12 -0300

    Add files via upload

---
+++ Atividade_06/Caio_Borba/6_caioborba.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+int main() {
+  int numero, incremento;
+  printf("Digite um numero: ");
+  scanf("%d", &numero);
+  printf("Digite um incremento: ");
+  scanf("%d", &incremento);
+
+  for (int contador = 0; contador <= numero; contador += incremento) {
+    printf("%d\n", contador);
+  }
+  return 0;
+}
