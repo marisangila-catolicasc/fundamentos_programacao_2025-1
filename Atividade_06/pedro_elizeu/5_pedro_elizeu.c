b460d2ba4b3c132c476a677a07c6891cbca365d1
Pedro39627 <p04.santos@catolicasc.edu.br>
2025-04-24 21:54:24 -0300

    Update 5_pedro_elizeu.c

--- Atividade_06/pedro_elizeu/5_pedro_elizeu.c
+++ Atividade_06/pedro_elizeu/5_pedro_elizeu.c
@@ -1,1 +1,13 @@
+#include <stdio.h>
 
+int main() {
+    int soma = 0;
+    
+    for(int i = 1; i <= 100; i++) {
+        soma += i;
+    }
+    
+    printf("Soma cem primeiros numeros naturais: %d\n", soma);
+    
+    return 0;
+}
