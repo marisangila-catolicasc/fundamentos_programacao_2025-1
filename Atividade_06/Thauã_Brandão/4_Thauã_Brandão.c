405560b88db089f576fd39bc91e972e60b814aea
ThauaBrandaoSC <thaua.silva@catolicasc.edu.br>
2025-04-25 19:28:20 -0300

    Add files via upload

---
+++ Atividade_06/Thauã_Brandão/4_Thauã_Brandão.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main() {
+    int resultado;
+  
+  for (int i = 0; i <= 100; i++) {
+    resultado += i;
+  }
+  
+  printf("Resultado: %d\n", resultado);
+    return 0;
+}
