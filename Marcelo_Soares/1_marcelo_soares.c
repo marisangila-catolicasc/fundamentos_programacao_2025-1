2ff91eea9c08cf5f59ec1a5a6e1d18c16a7f26d2
Marcelo Soares Filho <marcelo.soares@catolicasc.edu.br>
2025-04-25 19:16:44 -0300

    Delete Marcelo_Soares directory

--- Marcelo_Soares/1_marcelo_soares.c
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-int main(){
-
-    for (int i = 0; i < 8; i++)
-    {
-        printf("bom dia a todos\n");
-    }
-    return 0;
-}
--- Marcelo_Soares/3_marcelo_soares.c
+++
@@ -1,8 +0,0 @@
-#include <stdio.h>
-int main(){
-
-    for (int inteiro = 0; inteiro <= 100; inteiro += 5){
-        printf("%d\n", inteiro);
-    }
-        return 0;
-}
