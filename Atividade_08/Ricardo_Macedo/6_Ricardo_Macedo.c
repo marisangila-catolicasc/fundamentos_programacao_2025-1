cb5dcfbad0aee782eda4e9ab30cf1cd3da980402
RicardoMacedoFilho <ricardo.macedo@catolicasc.edu.br>
2025-05-02 17:21:24 -0300

    Add files via upload

---
+++ Atividade_08/Ricardo_Macedo/6_Ricardo_Macedo.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+int main() {
+    int lista[20], i, pares = 0;
+
+    printf("Digite 20 inteiros:\n");
+    for (i = 0; i < 20; i++) {
+        printf("Elemento %d: ", i + 1);
+        scanf("%d", &lista[i]);
+    }
+
+    for (i = 0; i < 20; i++) {
+        if (lista[i] % 2 == 0) {
+            pares++;
+        }
+    }
+
+    printf("Total de pares encontrados: %d\n", pares);
+
+    return 0;
+}
