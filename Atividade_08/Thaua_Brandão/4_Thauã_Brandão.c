d355f129baf3ee88c588800d78c7b40c5a0dfa07
ThauaBrandaoSC <thaua.silva@catolicasc.edu.br>
2025-05-02 18:08:36 -0300

    Add files via upload

---
+++ Atividade_08/Thaua_Brandão/4_Thauã_Brandão.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+int main() {
+    int vetor[10];
+    
+    printf("Digite 10 valores inteiros:\n");
+    for (int i = 0; i < 10; i++) {
+        scanf("%d", &vetor[i]);
+    }
+
+    printf("\nOrdem de entrada:\n");
+    for (int i = 0; i < 10; i++) {
+        printf("%d\n", vetor[i]);
+    }
+
+    printf("\nOrdem inversa:\n");
+    for (int i = 9; i >= 0; i--) {
+        printf("%d\n", vetor[i]);
+    }
+
+    return 0;
+}
