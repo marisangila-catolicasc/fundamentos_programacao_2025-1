67a361a43bcd1e4cee80555bfa7a8ee06ba68105
Dudadezotti <maria06.silva@catolicasc.edu.br>
2025-05-02 20:00:54 -0300

    Add files via upload

---
+++ Atividade_08/Maria_Eduarda_Dezotti/03_Maria_Eduarda_Dezotti.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+int main() {
+
+  int nums[4] = {874, 589, 811, 956};
+  float media;
+  float soma = 0;
+
+  for (int i = 0; i < 4; i++) {
+    soma += nums[i];
+  }
+
+  media = soma / 4;
+
+  printf("A media dos valores e: %.2f \n \n", media);
+
+  for (int i = 0; i < 4; i++) {
+    if (nums[i] > media) {
+      printf("Numeros acima da media: %d \n", nums[i]);
+    }
+  }
+  
+  return 0;
