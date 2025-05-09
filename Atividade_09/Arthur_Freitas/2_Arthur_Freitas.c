724d4b5412c27ed169dfd1506072881a6f11153c
arthurfreitas-csc <arthur.freitas@catolicasc.edu.br>
2025-05-09 11:24:59 -0300

    Add files via upload

---
+++ Atividade_09/Arthur_Freitas/2_Arthur_Freitas.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+int main() {
+    int matriz[3][3], linha;
+
+    for(int i = 0; i < 3; i++){
+        for(int j = 0; j < 3; j++){
+            printf("Digite 1 número para a posição [%d][%d]:\n", i, j);
+            scanf("%d", &matriz[i][j]);
+        }
+    
+    }
+
+    printf("Qual Linha você deseja observar?\n[0], [1] ou [2]\n");
+    scanf("%d", &linha);
+
+    printf("\nLinha [%d]: ", linha);
+    for (int j = 0; j < 3; j++) {
+        printf("[%d] ", matriz[linha][j]);
+    
+    }
+    return 0;
+}
