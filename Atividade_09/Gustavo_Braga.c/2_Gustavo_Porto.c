001ad64c6b1f97fa899c152fb5323d2a30b65721
GustavoBraga00 <gustavo.braga@catolicasc.edu.br>
2025-05-09 19:09:14 -0300

    Add files via upload

---
+++ Atividade_09/Gustavo_Braga.c/2_Gustavo_Porto.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+int main() {
+    int matriz[3][3];
+    int linha;
+
+    printf("Digite os valores de uma matriz 3x3 \n");
+    for (int i = 0; i < 3; i++) {
+        printf("Linha %d: ", i);
+        scanf("%d %d %d", &matriz[i][0], &matriz[i][1], &matriz[i][2]);
+    }
+
+    printf("Digite o numero da linha que deseja ver: 0, 1 ou 2 ");
+    scanf("%d", &linha);
+
+    if (linha >= 0 && linha < 3) {
+        printf("Valores da linha %d: ", linha);
+        for (int j = 0; j < 3; j++) {
+            printf("%d ", matriz[linha][j]);
+        }
+        printf("\n");
+    } else {
+        printf("Linha indisponivel. \n");
+    }
+
+    return 0;
+}
